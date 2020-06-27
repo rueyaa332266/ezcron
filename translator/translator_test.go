5d4a1244d87cf1f8816c1a8bea62a414d6cf3969
rueyaa332266 <rueyaa332266@gmail.com>
2020-06-28 00:00:26 +0900

    add some test

--- translator/translator_test.go
+++ translator/translator_test.go
@@ -17,7 +17,7 @@ func TestMatchCronReg(t *testing.T) {
 	if gotValid != true {
 		t.Errorf("got: %t; want: %t", gotValid, true)
 	}
-	for key, _ := range want {
+	for key := range want {
 		if !reflect.DeepEqual(gotCheckResults[key], want[key]) {
 			t.Errorf("Error in field: %s", key)
 		}
@@ -111,5 +111,62 @@ func TestCheckDayWeekReg(t *testing.T) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("Error in pattern: %s", wantList[i].Pattern)
 		}
+	}
+}
+
+func TestMonthToNum(t *testing.T) {
+	gotList := []string{"Jan", "FEB", "March", "APr", "MaY", "june", "July", "Aug", "Sep", "Oct", "Nov", "Dec"}
+	wantList := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}
+	for i := range gotList {
+		got := MonthToNum(gotList[i])
+		want := wantList[i]
+		if got != want {
+			t.Errorf("Got: %s, but want: %s", got, want)
+		}
+	}
+}
+
+func TestWeekDayToNum(t *testing.T) {
+	gotList := []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}
+	wantList := []string{"0", "1", "2", "3", "4", "5", "6"}
+	for i := range gotList {
+		got := WeekDayToNum(gotList[i])
+		want := wantList[i]
+		if got != want {
+			t.Errorf("Got: %s, but want: %s", got, want)
+		}
+	}
+}
+
+func TestOrdinalFromStr(t *testing.T) {
+	gotList := []string{"1", "2", "3", "4"}
+	wantList := []string{"1st", "2nd", "3rd", "4th"}
+	for i := range gotList {
+		got := OrdinalFromStr(gotList[i])
+		want := wantList[i]
+		if got != want {
+			t.Errorf("Got: %s, but want: %s", got, want)
+		}
+	}
+}
+
+func TestAddZeorforTenDigit(t *testing.T) {
+	gotList := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	wantList := []string{"00", "01", "02", "03", "04", "05", "06", "07", "08", "09"}
+	for i := range gotList {
+		got := AddZeorforTenDigit(gotList[i])
+		want := wantList[i]
+		if got != want {
+			t.Errorf("Got: %s, but want: %s", got, want)
+		}
+	}
+}
+
+func TestUniqueSlice(t *testing.T) {
+	slice := []string{"foo", "bar", "fizz", "foo", "fizz"}
+	got := uniqueSlice(slice)
+	want := []string{"foo", "bar", "fizz"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: %s, but want: %s", got, want)
 	}
 }
