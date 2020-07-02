b46faeeb0ad56bd78426af6a987779e89c210636
rueyaa332266 <rueyaa332266@gmail.com>
2020-07-03 00:02:04 +0900

    seperate slash explain

--- translator/translator.go
+++ translator/translator.go
@@ -16,6 +16,9 @@ type CheckResult struct {
 	Pattern string
 }
 
+var dayList = [7]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+var monthList = [12]string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+
 // Field name     Mandatory?   Allowed values    Allowed special characters
 // ----------     ----------   --------------    --------------------------
 // Minutes        Yes          0-59              * / , -
@@ -213,46 +216,50 @@ func explainMinute(c *CheckResult) string {
 		explanation += " every " + strings.Replace(minute, "-", " through ", 1) + " minute"
 	case "slash":
 		// "asterisk","number","comma","hyphen" / "number","comma"
-		minute := c.Input
-		var output string
-		slice := strings.Split(minute, "/")
-		for i, v := range slice {
-			CheckResult := CheckFieldeReg(v, "minute")
-			// check the left side of slash
-			if i == 0 {
-				switch CheckResult.Pattern {
-				case "asterisk":
-					output += ""
-				case "number":
-					if CheckResult.Input == "59" {
-						output += " from minute 59"
-					} else {
-						output += " from minute " + CheckResult.Input + " through minute 59"
-					}
-				case "comma":
-					minute := CheckResult.Input
-					slice := strings.Split(minute, ",")
-					for i, v := range slice {
-						if v == "59" {
-							slice[i] = "minute 59"
-						} else {
-							slice[i] = "minute " + v + " through minute 59"
-						}
-					}
-					output += " from " + strings.Join(slice, " and ")
-				case "hyphen":
-					minute := CheckResult.Input
-					output += " from minute " + strings.Replace(minute, "-", " through minute ", 1)
-				}
-			} else {
-				// check the right side of slash
-				// for pattern "number" and "comma"
-				output = CheckResult.Input + " minute" + output
-			}
-		}
-		explanation += " every " + output
+		explanation += " every " + explaiMinuteSlach(c)
 	}
 	return explanation
+}
+
+func explaiMinuteSlach(c *CheckResult) string {
+	minute := c.Input
+	var output string
+	slice := strings.Split(minute, "/")
+	for i, v := range slice {
+		CheckResult := CheckFieldeReg(v, "minute")
+		// check the left side of slash
+		if i == 0 {
+			switch CheckResult.Pattern {
+			case "asterisk":
+				output += ""
+			case "number":
+				if CheckResult.Input == "59" {
+					output += " from minute 59"
+				} else {
+					output += " from minute " + CheckResult.Input + " through minute 59"
+				}
+			case "comma":
+				minute := CheckResult.Input
+				slice := strings.Split(minute, ",")
+				for i, v := range slice {
+					if v == "59" {
+						slice[i] = "minute 59"
+					} else {
+						slice[i] = "minute " + v + " through minute 59"
+					}
+				}
+				output += " from " + strings.Join(slice, " and ")
+			case "hyphen":
+				minute := CheckResult.Input
+				output += " from minute " + strings.Replace(minute, "-", " through minute ", 1)
+			}
+		} else {
+			// check the right side of slash
+			// for pattern "number" and "comma"
+			output = CheckResult.Input + " minute" + output
+		}
+	}
+	return output
 }
 
 func explainHour(c *CheckResult) string {
@@ -279,52 +286,56 @@ func explainHour(c *CheckResult) string {
 		explanation += " from " + strings.Join(slice, "-")
 	case "slash":
 		// "asterisk","number","comma","hyphen" / "number","comma"
-		hour := c.Input
-		var output string
-		slice := strings.Split(hour, "/")
-		for i, v := range slice {
-			CheckResult := CheckFieldeReg(v, "hour")
-			// check the left side of slash
-			if i == 0 {
-				switch CheckResult.Pattern {
-				case "asterisk":
-					output += ""
-				case "number":
-					output += " from " + CheckResult.Input + ":00-24:00"
-				case "comma":
-					hour := CheckResult.Input
-					slice := strings.Split(hour, ",")
-					for i, v := range slice {
-						slice[i] = " " + AddZeorforTenDigit(v) + ":00-24:00"
-					}
-					output += " from" + strings.Join(slice, " and")
-				case "hyphen":
-					hour := CheckResult.Input
-					slice := strings.Split(hour, "-")
-					for i, v := range slice {
-						slice[i] = AddZeorforTenDigit(v) + ":00"
-					}
-					output += " from " + strings.Join(slice, "-")
-				}
-			} else {
-				// check the right side of slash
-				// for pattern "number" and "comma"
-				switch CheckResult.Pattern {
-				case "number":
-					output = "every " + CheckResult.Input + " hour" + output
-				case "comma":
-					hour := CheckResult.Input
-					slice := strings.Split(hour, ",")
-					for i, v := range slice {
-						slice[i] = "every " + v + " hour"
-					}
-					output = strings.Join(slice, " and ") + output
-				}
-			}
-		}
-		explanation += " " + output
+		explanation += " " + explainHourSlach(c)
 	}
 	return explanation
+}
+
+func explainHourSlach(c *CheckResult) string {
+	hour := c.Input
+	var output string
+	slice := strings.Split(hour, "/")
+	for i, v := range slice {
+		CheckResult := CheckFieldeReg(v, "hour")
+		// check the left side of slash
+		if i == 0 {
+			switch CheckResult.Pattern {
+			case "asterisk":
+				output += ""
+			case "number":
+				output += " from " + CheckResult.Input + ":00-24:00"
+			case "comma":
+				hour := CheckResult.Input
+				slice := strings.Split(hour, ",")
+				for i, v := range slice {
+					slice[i] = " " + AddZeorforTenDigit(v) + ":00-24:00"
+				}
+				output += " from" + strings.Join(slice, " and")
+			case "hyphen":
+				hour := CheckResult.Input
+				slice := strings.Split(hour, "-")
+				for i, v := range slice {
+					slice[i] = AddZeorforTenDigit(v) + ":00"
+				}
+				output += " from " + strings.Join(slice, "-")
+			}
+		} else {
+			// check the right side of slash
+			// for pattern "number" and "comma"
+			switch CheckResult.Pattern {
+			case "number":
+				output = "every " + CheckResult.Input + " hour" + output
+			case "comma":
+				hour := CheckResult.Input
+				slice := strings.Split(hour, ",")
+				for i, v := range slice {
+					slice[i] = "every " + v + " hour"
+				}
+				output = strings.Join(slice, " and ") + output
+			}
+		}
+	}
+	return output
 }
 
 func explainDayMonth(c *CheckResult) string {
@@ -343,47 +354,50 @@ func explainDayMonth(c *CheckResult) string {
 		explanation = "between day " + strings.Replace(dayM, "-", " and ", -1) + " of the month"
 	case "slash":
 		// "asterisk","number","comma","hyphen" / "number","comma"
-		dayM := c.Input
-		var output string
-		slice := strings.Split(dayM, "/")
-		for i, v := range slice {
-			CheckResult := CheckFieldeReg(v, "dayM")
-			// check the left side of slash
-			if i == 0 {
-				switch CheckResult.Pattern {
-				case "asterisk":
-					output += ""
-				case "number":
-					output += " from day " + CheckResult.Input + " of the month"
-				case "comma":
-					dayM := CheckResult.Input
-					slice := strings.Split(dayM, ",")
-					for i, v := range slice {
-						slice[i] = "day " + v + " of the month"
-					}
-					output += " from " + strings.Join(slice, " and ")
-				case "hyphen":
-					dayM := CheckResult.Input
-					slice := strings.Split(dayM, "-")
-					for i, v := range slice {
-						slice[i] = "day " + v
-					}
-					output += " between " + strings.Join(slice, " and ") + " of the month"
-				}
-			} else {
-				// check the right side of slash
-				// for pattern "number" and "comma"
-				output = "every " + CheckResult.Input + " day of month" + output
-			}
-		}
-		explanation += " " + output
+		explanation += " " + explainDayMonthSlach(c)
 	}
 	return explanation
 }
 
+func explainDayMonthSlach(c *CheckResult) string {
+	dayM := c.Input
+	var output string
+	slice := strings.Split(dayM, "/")
+	for i, v := range slice {
+		CheckResult := CheckFieldeReg(v, "dayM")
+		// check the left side of slash
+		if i == 0 {
+			switch CheckResult.Pattern {
+			case "asterisk":
+				output += ""
+			case "number":
+				output += " from day " + CheckResult.Input + " of the month"
+			case "comma":
+				dayM := CheckResult.Input
+				slice := strings.Split(dayM, ",")
+				for i, v := range slice {
+					slice[i] = "day " + v + " of the month"
+				}
+				output += " from " + strings.Join(slice, " and ")
+			case "hyphen":
+				dayM := CheckResult.Input
+				slice := strings.Split(dayM, "-")
+				for i, v := range slice {
+					slice[i] = "day " + v
+				}
+				output += " between " + strings.Join(slice, " and ") + " of the month"
+			}
+		} else {
+			// check the right side of slash
+			// for pattern "number" and "comma"
+			output = "every " + CheckResult.Input + " day of month" + output
+		}
+	}
+	return output
+}
+
 func explainMonth(c *CheckResult) string {
 	explanation := "in"
-	monthList := [12]string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
 	switch c.Pattern {
 	case "asterisk":
 		explanation = ""
@@ -406,48 +420,51 @@ func explainMonth(c *CheckResult) string {
 		explanation += " every month from " + strings.Join(slice, " through ")
 	case "slash":
 		// "asterisk","number","comma","hyphen" / "number","comma"
-		month := c.Input
-		var output string
-		slice := strings.Split(month, "/")
-		for i, v := range slice {
-			CheckResult := CheckFieldeReg(v, "month")
-			// check the left side of slash
-			if i == 0 {
-				switch CheckResult.Pattern {
-				case "asterisk":
-					output += ""
-				case "number":
-					monthNum, _ := strconv.Atoi(CheckResult.Input)
-					output += " from " + monthList[monthNum-1]
-				case "comma":
-					slice := strings.Split(CheckResult.Input, ",")
-					for i, v := range slice {
-						monthNum, _ := strconv.Atoi(v)
-						slice[i] = monthList[monthNum-1]
-					}
-					output += " from " + strings.Join(slice, " and ")
-				case "hyphen":
-					slice := strings.Split(CheckResult.Input, "-")
-					for i, v := range slice {
-						monthNum, _ := strconv.Atoi(v)
-						slice[i] = monthList[monthNum-1]
-					}
-					output += " from " + strings.Join(slice, " through ")
-				}
-			} else {
-				// check the right side of slash
-				// for pattern "number" and "comma"
-				output = "every " + CheckResult.Input + " month" + output
-			}
-		}
-		explanation += " " + output
+		explanation += " " + explainMonthSlach(c)
 	}
 	return explanation
 }
 
+func explainMonthSlach(c *CheckResult) string {
+	month := c.Input
+	var output string
+	slice := strings.Split(month, "/")
+	for i, v := range slice {
+		CheckResult := CheckFieldeReg(v, "month")
+		// check the left side of slash
+		if i == 0 {
+			switch CheckResult.Pattern {
+			case "asterisk":
+				output += ""
+			case "number":
+				monthNum, _ := strconv.Atoi(CheckResult.Input)
+				output += " from " + monthList[monthNum-1]
+			case "comma":
+				slice := strings.Split(CheckResult.Input, ",")
+				for i, v := range slice {
+					monthNum, _ := strconv.Atoi(v)
+					slice[i] = monthList[monthNum-1]
+				}
+				output += " from " + strings.Join(slice, " and ")
+			case "hyphen":
+				slice := strings.Split(CheckResult.Input, "-")
+				for i, v := range slice {
+					monthNum, _ := strconv.Atoi(v)
+					slice[i] = monthList[monthNum-1]
+				}
+				output += " from " + strings.Join(slice, " through ")
+			}
+		} else {
+			// check the right side of slash
+			// for pattern "number" and "comma"
+			output = "every " + CheckResult.Input + " month" + output
+		}
+	}
+	return output
+}
+
 func explainDayWeek(c *CheckResult) string {
 	explanation := "on"
-	dayList := [7]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 	switch c.Pattern {
 	case "asterisk":
 		explanation = ""
@@ -470,45 +487,49 @@ func explainDayWeek(c *CheckResult) string {
 		explanation += " every day from " + strings.Join(slice, " through ")
 	case "slash":
 		// "asterisk","number","comma","hyphen" / "number","comma"
-		dayW := c.Input
-		var output string
-		slice := strings.Split(dayW, "/")
-		for i, v := range slice {
-			CheckResult := CheckFieldeReg(v, "dayW")
-			// check the left side of slash
-			if i == 0 {
-				switch CheckResult.Pattern {
-				case "asterisk":
-					output += ""
-				case "number":
-					dayNum, _ := strconv.Atoi(CheckResult.Input)
-					output += " from " + dayList[dayNum]
-				case "comma":
-					dayW := CheckResult.Input
-					slice := strings.Split(dayW, ",")
-					for i, v := range slice {
-						dayNum, _ := strconv.Atoi(v)
-						slice[i] = dayList[dayNum]
-					}
-					output += " from " + strings.Join(slice, " and ")
-				case "hyphen":
-					dayW := CheckResult.Input
-					slice := strings.Split(dayW, "-")
-					for i, v := range slice {
-						dayNum, _ := strconv.Atoi(v)
-						slice[i] = dayList[dayNum]
-					}
-					output += " between " + strings.Join(slice, " and ")
-				}
-			} else {
-				// check the right side of slash
-				// for pattern "number" and "comma"
-				output = "every " + CheckResult.Input + " day of week" + output
-			}
-		}
-		explanation += " " + output
+		explanation += " " + explainDayWeekSlach(c)
 	}
 	return explanation
+}
+
+func explainDayWeekSlach(c *CheckResult) string {
+	dayW := c.Input
+	var output string
+	slice := strings.Split(dayW, "/")
+	for i, v := range slice {
+		CheckResult := CheckFieldeReg(v, "dayW")
+		// check the left side of slash
+		if i == 0 {
+			switch CheckResult.Pattern {
+			case "asterisk":
+				output += ""
+			case "number":
+				dayNum, _ := strconv.Atoi(CheckResult.Input)
+				output += " from " + dayList[dayNum]
+			case "comma":
+				dayW := CheckResult.Input
+				slice := strings.Split(dayW, ",")
+				for i, v := range slice {
+					dayNum, _ := strconv.Atoi(v)
+					slice[i] = dayList[dayNum]
+				}
+				output += " from " + strings.Join(slice, " and ")
+			case "hyphen":
+				dayW := CheckResult.Input
+				slice := strings.Split(dayW, "-")
+				for i, v := range slice {
+					dayNum, _ := strconv.Atoi(v)
+					slice[i] = dayList[dayNum]
+				}
+				output += " between " + strings.Join(slice, " and ")
+			}
+		} else {
+			// check the right side of slash
+			// for pattern "number" and "comma"
+			output = "every " + CheckResult.Input + " day of week" + output
+		}
+	}
+	return output
 }
 
 func MonthToNum(str string) string {
